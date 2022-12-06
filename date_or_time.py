89d565b21a76ac28f9a8ed9951b91f9d8cf0a49c
陈文超 <chenwc@cffex.com.cn>
2022-12-06 09:42:43 +0800

    '新增将时间戳转为字符串日期功能'

--- date_or_time.py
+++ date_or_time.py
@@ -25,3 +25,12 @@ def date_format_validate(date_text, date_format):
         return datetime.datetime.strptime(date_text, date_format).date()
     except ValueError:
         return False
+
+def time_num_to_string(number, format_string='%Y-%m-%d %H:%M:%S'):
+    """
+    将数字格式的时间戳转换为字符串格式, 默认为
+    :param number:
+    :param format_string:
+    :return:
+    """
+    return datetime.fromtimestamp(number).strftime(format_string)
